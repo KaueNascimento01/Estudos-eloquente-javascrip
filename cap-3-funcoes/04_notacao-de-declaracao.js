79202bbe769a1147c9c5a5fc333122d968c1b6fe
Kauê Nascimento <kaue.sc78@gmail.com>
2026-06-29 19:09:08 -0300

    feat(cap-3): adicionar declaracao de funcao

---
+++ cap-3-funcoes/04_notacao-de-declaracao.js
@@ -0,0 +1,12 @@
+//declaracao de funcao
+function square(x){
+    return x * x;
+}
+
+console.log(square(10))
+
+console.log("The future says:", future());
+
+function future(){
+    return "You'll never have flying cars"
+}
