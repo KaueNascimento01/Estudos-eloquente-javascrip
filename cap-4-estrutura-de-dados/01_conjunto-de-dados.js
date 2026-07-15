bda7dc3ef3894cf106546e81c5439eb9d98b55e1
Kauê Nascimento <kaue.sc78@gmail.com>
2026-07-14 23:35:23 -0300

    feat: add access to properties of arrays and objects

---
+++ cap-4-estrutura-de-dados/01_conjunto-de-dados.js
@@ -0,0 +1,13 @@
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]); // 5
+console.log(listOfNumbers[0]); // 2
+console.log(listOfNumbers[2 - 1]); // 3
+
+let user = {
+  name: 'Kauê',
+  age: 19
+};
+
+let key = 'name';
+
+console.log(user[key]); // Kauê
