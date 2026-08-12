d6b739c6763b60515f22292bde21b6dd7daa6ef2
Kauê Nascimento <kaue.sc78@gmail.com>
2026-08-12 19:43:40 -0300

    feat: demonstrate max function and spread operator usage

---
+++ cap-4-estrutura-de-dados/09_parametro-rest.js
@@ -0,0 +1,20 @@
+function max(...numbers) {
+    let result = -Infinity;
+    for (let number of numbers) {
+    if (number > result) result = number;
+    }
+    return result;
+}
+
+console.log(max(4, 1, 9, -2)); // 9
+
+let numbers = [5, 1, 7];
+
+console.log(max(...numbers)); // 7
+
+let words = ["never", "fully"];
+console.log(["will", ...words, "understand"]); // [ 'will', 'never', 'fully', 'understand' ]
+
+let coordinates = {x: 10, y: 0};
+// Criação de um novo objeto 
+console.log({...coordinates, y: 5, z: 1}); // { x: 10, y: 5, z: 1 }
